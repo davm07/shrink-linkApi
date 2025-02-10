a066acda5635caf52ee2ed051a029f13c19cabfe
Diego Vasquez <122320757+davm07@users.noreply.github.com>
2025-02-11 00:13:09 +0100

    add endpoint to get user by user id

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const express = require('express');
+import dotenv from 'dotenv';
+import express from 'express';
+import { connectDB } from './lib/db.js';
+import urlSchema from './models/urlSchema.js';
+
+dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 7000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/api/urls/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  const urlInfo = await urlSchema.find({ userId });
+  res.send(urlInfo);
 });
---
+++ models/urlSchema.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import { nanoid } from 'nanoid';
+
+const urlSchema = new mongoose.Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true
+    },
+    shortenedUrl: {
+      type: String,
+      required: true,
+      unique: true,
+      default: nanoid(10)
+    },
+    userId: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { collection: 'urlShort' }
+);
+
+export default mongoose.model('urlShort', urlSchema);
